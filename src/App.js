f59bf34e27520236a604b3f83af7e19eb557bdb1
icerittikhai35 <ritthikai.khan@gmai.com>
2022-05-18 17:52:34 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [loading, setLoading] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const loadPost = async () => {
+            setLoading(true);
+            const response = await axios.get("https://reqres.in/api/users");
+            setPosts(response.data.data);
+            setLoading(false);
+        }
+
+        // Call the function
+        loadPost();
+    }, []);
+
+
+
+
+    return (
+        <>
+            <div className="App">
+                {loading ?
+                    (
+                        <h4>Loading...</h4>
+                    ) :
+                    (posts.map((item) =>
+
+                        <div className='border' >
+                            <div>
+                                <h4>{item.first_name}</h4>
+                            </div>
+                            <div >
+                                <img src={item.avatar} style={{ maxHeight: '300px', maxWidth: '300px', borderRadius: '50%' }} />
+                                <h4>Name: {item.first_name}   {item.last_name}</h4>
+                                <h4>Email: {item.email}</h4>
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    );
 }
 
 export default App;
